c69ad3bf72849698a355ee0c157d30962934abf7
Matthew Courtney <matthewdc182@gmail.com>
2021-01-04 21:31:38 -0500

    installed dependencies and got the inquirer prompts going

---
+++ index.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+
+inquirer.prompt([
+    {
+        type: "input",
+        message: "What is your name?",
+        name: "name"
+    },
+    {
+        type: "input",
+        message: "Provide a brief description of yourself",
+        name: "bio"
+    },
+    {
+        type: "input",
+        message: "Where do you live?",
+        name: "location"
+    },
+    {
+        type: "input",
+        message: "please provide an web based image source?",
+        name: "img"
+    },
+    {
+        type: "checkbox",
+        message: "What programing languages do you know?",
+        choices: ["HTML", "CSS", "JavaScript", "JQuery", "Node.js"],
+        name: "languages"
+    },
+]).then(response => console.log(response))
